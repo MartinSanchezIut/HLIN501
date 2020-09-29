09d949ea7aefa935de9421518b7f91af2b841974
Martin <martin.sanchez@etu.umontpellier.fr>
2020-09-29 11:09:01 +0200

    Fin tp1

--- TP1/untitled.cpp
+++ TP1/untitled.cpp
@@ -5,6 +5,34 @@
 
 using namespace std;
 
+void ecrireComposantes(int n, int m, int comp[]) {
+	cout << endl;
+
+	for (int a = 1; a < n + 1; ++a){
+		int x = 0;
+		//cout << "Recherche des " << a << " conexités" << endl ;
+
+		for (int i = 0; i < n; ++i){
+			int goal = comp[i] ;
+			int count = 0;
+			//cout << "On cherche comp[" << i << "] = " << comp[i] << endl;
+
+			for (int j = 0; j < n; ++j){
+				if(comp[j] == goal) {
+					count++ ;
+					//cout << " " << count << " ";
+				}
+			}
+			//cout << endl;
+
+			if (count == a) {
+				x++;
+			}
+		}
+		cout << "Composantes de " << a << " sommets = " << x/a << endl ;
+	}	
+	cout << endl;
+}
 
 void grapheRandom(int n , int m , int edge[][2]) {
 	cout << endl << "Graphe Random" <<endl;
@@ -41,20 +69,29 @@ void composantes(int n, int m, int edge[][2], int comp[]) {
 
 
 		if ( comp[x] != comp[y] ){ 
-			int aux = comp[x];
+			int min, max;
+			if (comp[x] > comp[y]) {
+				max = comp[x];
+				min = comp[y];
+			}else {
+				max = comp[y];
+				min = comp[x];
+			}
+
+			//int aux = comp[x];
 			//cout << "Comp(" << x << ") : " << aux << endl;
 
 			for ( int z = 0 ; z < n ; z++ ) {
-				cout << "Comp(" << z << ") : " << comp[z] ;
+			//	cout << "Comp(" << z << ") : " << comp[z] ;
 
-				if ( comp[z] == comp[y] ) {
-					comp[z] = aux;
-					cout << "  (modif)" << endl;		
+				if ( comp[z] == max ) {  	 	// ( comp[z] == comp[y] )  
+					comp[z] = min;					// 	comp[z] = aux;
+			//		cout << "  (modif)" << endl;		
 				}else {
-					cout << endl;
+			//		cout << endl;
 				}
 			}
-			cout << endl ;
+			//cout << endl ;
 		}
 		cout << endl;
 	}
@@ -67,19 +104,20 @@ void composantes(int n, int m, int edge[][2], int comp[]) {
 
 
 int main() {
-	int n = 4;     // Nombre de sommets.
-	int m = 6;     // Nombre d’aretes.
+	int n;     // Nombre de sommets.
+	int m;     // Nombre d’aretes.
 	
-	cout << "Entrer le nombre de sommets:  (4) " << endl;
-	//cin >> n;
-	cout << "Entrer le nombre d’aretes:  (6) " ;
-	//cin >> m;
+	cout << "Entrer le nombre de sommets:  " << endl;
+	cin >> n;
+	cout << "Entrer le nombre d’aretes:   " ;
+	cin >> m;
 
 	int edge[m][2];    // Tableau des aretes.
 	int comp[n]; // comp[i] est le numero de la composante contenant i.
 
 	grapheRandom(n,m, edge);
 	composantes(n,m, edge, comp);
+	ecrireComposantes(n, m, comp) ;
 
 	return EXIT_SUCCESS;
 }
