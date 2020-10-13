a896030ff1de88bccf2bc59c0a552852c4cc9b92
Martin <martin.sanchez@etu.umontpellier.fr>
2020-10-13 11:16:53 +0200

    avancement

--- TP2/tp2.cc
+++ TP2/tp2.cc
@@ -9,48 +9,63 @@ using namespace std;
 //typedef struct coord{int abs; int ord;} coord;
 
 
-
-
-void pointRandom(int n, coord points[]) {
+void pointRandom(int n, coord point[]) {
 	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		point[i].abs = rand()%613 ; 
+		point[i].ord = rand()%793 ; 
 
-	for (int i = 0; i < n; ++i) {
-		points[i].abs = rand()%613 ; 
-		points[i].ord = rand()%793 ; 
-
-		cout << i << " : " << points[i].abs << " " <<	points[i].ord << endl ; 
+		//cout << i << " : " << point[i].abs << " " <<	point[i].ord << endl ; 
 	}
+}
+int carre(int n) {
+	return n * n;
 }
 
 void distances(int n, int m, coord points[], int edge[][3]) {
-
+	int last = 0;
 	for (int i = 0; i < n; ++i)	{
-		for (int j = 0; j < n; j++){
-			
-			edge[i][0] = i;
-			edge[i][1] = j;
-			edge[i][2] =  (points[i].abs - points[j].abs)^2 + (points[i].ord - points[j].ord)^2 ;
+		for (int j = i+1; j < n; j++){
+			//cout << last << " " <<i <<" " << j << endl ;
+			edge[last][0] = i;
+			edge[last][1] = j;
+			edge[last][2] =  carre(points[i].abs - points[j].abs) + carre(points[i].ord - points[j].ord) ;
 
-
-			cout << "Distance : " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
-
+			last++;
 		}
 	}
+	/*
+	for (int i = 0; i < m; ++i)	{
+		cout << "Distance : " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
+
+	}
+	*/
 }
 
-int compare(const void * a,const void * b){ return ((int*)a)[2] - ((int*)b)[2]; }
+int compare(const void * a,const void * b){
+	int ret = ( (int*) a)[2] - ( (int*) b)[2] ; 
+	//cout << ( (int*) a)[2] << " - " << ( (int*) b)[2] << " = " << ret << endl;
+	return ret; 
+}
 
 void tri(int m, int edge[][3]) {
-	qsort(edge, m, sizeof(edge[0]), compare);
-
 	for (int i = 0; i < m; i++){
-		cout << "Tri : " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
+	 	cout << "PreTri " << i << ": " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
+	}
+
+	qsort(edge, m, sizeof(edge[0]), compare);
+	cout << endl ;
+	for (int i = 0; i < m; i++){
+	 	cout << "Tri " << i << ": " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
 	}
 }
 
 
 void kruskal(int n , int edge[][3] , int arbre[][2]) {
-	tri(n, edge) ;
+	cout << "Debut kruskal " << endl;
+	int m=n*(n-1)/2;   // Le nombre de paires de points.
+
+	tri(m, edge) ;
 	// arbre est vide
 
 	int comp[n] ;
@@ -58,8 +73,17 @@ void kruskal(int n , int edge[][3] , int arbre[][2]) {
 		comp[i] = i;
 	}
 
-	int m=n*(n-1)/2;   // Le nombre de paires de points.
+	cout << endl;
+	cout << "Tri + comp : Ok" << endl;
+	cout << "M = " << m << endl ;
+
 	for (int i = 0; i < m; ++i)	{
+		cout << "Tour:" << i << endl;
+		cout << "Edge : " << edge[i][0] << " " << edge[i][1] << " " << edge[i][2] << endl;
+		//cout << "Comp : " << comp[edge[i][0]]] << " " << comp[edge[i][1]]] << endl;
+		cout << endl;
+		cout << endl;
+
 		if (comp[edge[i][0]]  !=  comp[edge[i][1]]) {
 
 			int aux = comp[edge[i][0]];
@@ -79,28 +103,35 @@ void kruskal(int n , int edge[][3] , int arbre[][2]) {
 
 
 int main() {
-	int n = 50;             //Le nombre de points.
-	cout << "Entrer le nombre de points:   =50" << endl;
+	int n = 5;             //Le nombre de points.
+	cout << "Entrer le nombre de points:   =5" << endl;
 	//cin >> n;
 
 	int m=n*(n-1)/2;   // Le nombre de paires de points.
 
  
 	coord point[n];    // Les coordonnees des points dans le plan.
-
 	int edge[m][3];    // Les paires de points et le carre de leur longueur.
-
 	int arbre[n-1][2]; // Les aretes de l'arbre de Kruskal. 
 
 
 
-	pointRandom(n, point) ; cout << endl;
-	distances(n, m, point, edge) ; cout << endl;
+	pointRandom(n, point) ;
+	for (int i = 0; i < n; ++i)	{
+		cout << "Point[" << i << "] : " <<  point[i].abs << " - " << point[i].ord << endl ;
+	}
+	
+	distances(n, m, point, edge) ; 
+	cout << endl;
+	for (int i = 0; i < m; i++){
+	 	cout << "Edge[" << i << "]: " << edge[i][0] << "-" << edge[i][1] << " = " << edge[i][2] << endl ; 
+	}
+	cout << endl;
+	
 	tri(n, edge) ;
-
 	//kruskal(n, edge, arbre) ;
 
-	affichageGraphique(n, point, arbre) ;
+	//affichageGraphique(n, point, arbre) ;
 
 	return EXIT_SUCCESS;
 }
